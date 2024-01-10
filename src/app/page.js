ccaba32fb488d5e6456606f551e94e7f95434a10
abe valle <abe@valle.us>
2024-01-09 22:07:02 -0600

    start of next project

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1 align='center'>ZipMyFiles.com</h1>
+      <h3 align='center'>Coming Soon!</h3>
+    </main>
+  )
+}
